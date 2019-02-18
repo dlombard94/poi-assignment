cb80dcc9cd497647293e3e4f22de04dffc8c0b22
DLOMBARD\Darragh <112500343@umail.ucc.ie>
2019-02-18 22:16:23 +0000

    Introduce and Serve Static Assets

--- routes.js
+++ routes.js
@@ -1,3 +1,13 @@
 const Pois = require('./app/controllers/pois');
 
-module.exports = [{ method: 'GET', path: '/', config: Pois.index }];
+module.exports = [{ method: 'GET', path: '/', config: Pois.index },
+    {
+        method: 'GET',
+        path: '/{param*}',
+        handler: {
+            directory: {
+                path: './public'
+            }
+        }
+    }
+    ];
