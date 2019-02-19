2131f95a0abc12a6c70d244d3c338b638aa786db
DLOMBARD\Darragh <112500343@umail.ucc.ie>
2019-02-19 19:49:47 +0000

    Added signup and login page

--- routes.js
+++ routes.js
@@ -1,6 +1,8 @@
 const Pois = require('./app/controllers/pois');
 
 module.exports = [{ method: 'GET', path: '/', config: Pois.index },
+    { method: 'GET', path: '/signup', config: Pois.signup },
+    { method: 'GET', path: '/login', config: Pois.login },
     {
         method: 'GET',
         path: '/{param*}',
