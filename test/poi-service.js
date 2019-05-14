6c900fd2cc682b684dda552ad15e000f0d3fae1e
DLOMBARD\Darragh <112500343@umail.ucc.ie>
2019-05-14 21:28:47 +0100

    added poi-service.js

---
+++ test/poi-service.js
@@ -0,0 +1,61 @@
+'use strict';
+
+const axios = require('axios');
+const baseUrl = 'http://dlombard:3000';
+
+class PoiService {
+    constructor(baseUrl) {
+        this.baseUrl = baseUrl;
+    }
+
+    async getIslands() {
+        const response = await axios.get(this.baseUrl + '/api/islands');
+        return response.data;
+    }
+
+    async getIsland(id) {
+        try {
+            const response = await axios.get(this.baseUrl + '/api/islandss/' + id);
+            return response.data;
+        } catch (e) {
+            return null;
+        }
+    }
+
+    async createIsland(newIsland) {
+        const response = await axios.post(this.baseUrl + '/api/islands', newIsland);
+        return response.data;
+    }
+
+    async deleteAllIslands() {
+        const response = await axios.delete(this.baseUrl + '/api/islands');
+        return response.data;
+    }
+
+    async deleteOneIsland(id) {
+        const response = await axios.delete(this.baseUrl + '/api/islands/' + id);
+        return response.data;
+    }
+
+    async getUsers() {
+        const response = await axios.get(this.baseUrl + '/api/users');
+        return response.data;
+    }
+
+    async getUser(id) {
+        try {
+            const response = await axios.get(this.baseUrl + '/api/user/' + id);
+            return response.data;
+            return response.data;
+        } catch (e) {
+            return null;
+        }
+    }
+
+    async createUser(newUser) {
+        const response = await axios.post(this.baseUrl + '/api/users', newUser);
+        return response.data;
+    }
+}
+
+module.exports = PoiService;
