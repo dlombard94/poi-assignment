8e55eb8ec88df9b8e0ddd6e6188b05550439ef04
DLOMBARD\Darragh <112500343@umail.ucc.ie>
2019-05-20 18:02:15 +0100

    added authenticate intial tests

---
+++ test/authapitest.js
@@ -0,0 +1,36 @@
+'use strict';
+
+const assert = require('chai').assert;
+const PoiService = require('./poi-service');
+const fixtures = require('./fixtures.json');
+const utils = require('../app/api/utils.js');
+
+suite('Auth API tests', function () {
+
+    let users = fixtures.users;
+    let newUser = fixtures.newUser;
+
+    const poiService = new PoiService('http://Dlombard:3000');
+
+    setup(async function () {
+        await poiService.deleteAllUsers();
+    });
+
+    test('authenticate', async function () {
+        const returnedUser = await poiService.createUser(newUser);
+        console.log(returnedUser);
+        const response = await poiService.authenticate(newUser);
+        console.log(response);
+        assert(response.success);
+        assert.isDefined(response.token);
+    });
+
+    test('verify Token', async function () {
+        const returnedUser = await poiService.createUser(newUser);
+        const response = await poiService.authenticate(newUser);
+
+        const userInfo = utils.decodeToken(response.token);
+        assert.equal(userInfo.email, returnedUser.email);
+        assert.equal(userInfo.userId, returnedUser._id);
+    });
+});
